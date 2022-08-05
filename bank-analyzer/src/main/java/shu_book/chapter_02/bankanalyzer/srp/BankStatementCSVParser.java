0960063e61250914c5922bb6ef35d09cfb6bc890
Vitaliy Oleinik <oleynik624@gmail.com>
2022-08-06 01:21:24 +0600

    write BankTransactional entity for bank analyzer, added new class for parser CSV

---
+++ bank-analyzer/src/main/java/shu_book/chapter_02/bankanalyzer/srp/BankStatementCSVParser.java
@@ -0,0 +1,9 @@
+package shu_book.chapter_02.bankanalyzer.srp;
+
+import java.time.format.DateTimeFormatter;
+
+public class BankStatementCSVParser {
+    private static final DateTimeFormatter DATE_PATTERN
+            = DateTimeFormatter. ofPattern ("dd-MM-yyyy") ;
+
+}
