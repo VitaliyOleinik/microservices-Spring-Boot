24d79e76eb8e766968ac8711575392afb92eabf9
Vitaliy Oleinik <oleynik624@gmail.com>
2022-08-10 21:31:10 +0600

    refactoring

--- bank-analyzer/src/main/java/shu_book/chapter_02/bankanalyzer/srp/BankStatementCSVParser.java
+++ bank-analyzer/src/main/java/shu_book/chapter_02/bankanalyzer/srp/BankStatementCSVParser.java
@@ -5,11 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankStatementCSVParser implements BankStatementParser{
+public class BankStatementCSVParser implements BankStatementParser {
     private static final DateTimeFormatter DATE_PATTERN
-            = DateTimeFormatter. ofPattern ("dd-MM-yyyy") ;
+            = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-    private BankTransaction parseFromCSV (final String line) {
+    @Override
+    public BankTransaction parserFrom(String line) {
         final String[] columns = line.split(",");
 
         final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
@@ -19,21 +20,12 @@ public class BankStatementCSVParser implements BankStatementParser{
         return new BankTransaction(date, amount, description);
     }
 
-    public List<BankTransaction> parseLinesFromCSV (final List<String> lines) {
-        final List<BankTransaction> bankTransactions = new ArrayList<>();
-        for (final String line: lines) {
-            bankTransactions.add(parseFromCSV(line));
-        }
-        return bankTransactions;
-    }
-
-    @Override
-    public BankTransaction parserFrom(String line) {
-        return parseFromCSV(line);
-    }
-
     @Override
     public List<BankTransaction> parseLinesFrom(List<String> lines) {
-        return parseLinesFromCSV(lines);
+        final List<BankTransaction> bankTransactions = new ArrayList<>();
+        for (final String line: lines) {
+            bankTransactions.add(parserFrom(line));
+        }
+        return bankTransactions;
     }
 }
