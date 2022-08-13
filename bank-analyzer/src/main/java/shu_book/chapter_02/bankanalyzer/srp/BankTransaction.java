fff01a64615b259357446d99d4437ea70c55e282
Vitaliy Oleinik <oleynik624@gmail.com>
2022-08-13 22:58:59 +0600

    добавлен ШАБЛОН УВЕДОМЛЕНИЯ для валидации данных класса BankTransaction

--- bank-analyzer/src/main/java/shu_book/chapter_02/bankanalyzer/srp/BankTransaction.java
+++ bank-analyzer/src/main/java/shu_book/chapter_02/bankanalyzer/srp/BankTransaction.java
@@ -1,6 +1,9 @@
 package shu_book.chapter_02.bankanalyzer.srp;
 
+import shu_book.chapter_02.bankanalyzer.srp.ocp.exception.Notification;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 public class BankTransaction {
@@ -24,6 +27,35 @@ public class BankTransaction {
 
     public String getDescription() {
         return description;
+    }
+
+    public Notification validate() {
+
+        final Notification notification = new Notification();
+        if (this.description.length() > 100) {
+            notification.addError("The description is too long");
+        }
+
+        final LocalDate parseDate;
+        try {
+            parseDate = LocalDate.parse(this.date.toString());
+            if (parseDate.isAfter(LocalDate.now())) {
+                notification.addError("date cannot be in the future");
+            }
+        }
+        catch (DateTimeParseException e) {
+            notification.addError("Invalid format for date");
+        }
+
+        final double amount;
+
+        try {
+            amount = Double.parseDouble(String.valueOf(this.amount));
+        }
+        catch (NumberFormatException e) {
+            notification.addError("Invalid format for amount");
+        }
+        return notification;
     }
 
     @Override
