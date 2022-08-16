f6ea5a06d56723cd1765a159426cd43c227d55cd
Vitaliy Oleinik <oleynik624@gmail.com>
2022-08-16 23:01:26 +0600

    prepare for LCP

--- bank-analyzer/src/main/java/shu_book/chapter_03/lcp/DocumentManagementSystem.java
+++ bank-analyzer/src/main/java/shu_book/chapter_03/lcp/DocumentManagementSystem.java
@@ -1,7 +1,9 @@
 package shu_book.chapter_03.lcp;
 
 import javax.swing.text.Document;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DocumentManagementSystem {
     void importFile(String path) {
@@ -10,4 +12,13 @@ public class DocumentManagementSystem {
     List<Document> contents() {
         return null;
     }
+
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem() {
+        extensionToImporter.put("letter", new LetterImporter());
+        extensionToImporter.put("report", new ReportImporter());
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+
 }
