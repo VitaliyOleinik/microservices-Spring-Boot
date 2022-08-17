351fe7881f74e78c8b74cdf8f44b490e4117a86d
Vitaliy Oleinik <oleynik624@gmail.com>
2022-08-17 20:15:40 +0600

    prepare for LCP

--- bank-analyzer/src/main/java/shu_book/chapter_03/lcp/DocumentManagementSystem.java
+++ bank-analyzer/src/main/java/shu_book/chapter_03/lcp/DocumentManagementSystem.java
@@ -1,24 +1,49 @@
 package shu_book.chapter_03.lcp;
 
-import javax.swing.text.Document;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DocumentManagementSystem {
-    void importFile(String path) {
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+    private final ArrayList<Document> documents = new ArrayList<>();
+
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found For file: "
+                        + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found For file: " +
+                    path);
+        }
     }
 
     List<Document> contents() {
         return null;
     }
 
-    private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem() {
         extensionToImporter.put("letter", new LetterImporter());
         extensionToImporter.put("report", new ReportImporter());
         extensionToImporter.put("jpg", new ImageImporter());
     }
-
 }
